e194a827fe9e48eeee1c742ad91f666ff13ff7b2
Quách Vinh Quang <hynoras1@gmail.com>
2025-02-14 16:01:30 +0700

    styling login page

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,30 +1,37 @@
-import "./style.scss"
+import React from "react"
+import { Button, Typography } from "@mui/material"
+import { Input, Space } from "antd"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons"
+import "./style.scss"
 
-const Login = () => {
+const Login: React.FC = () => {
   return (
-    <body>
-      <div className={"login-container"}>
-        <form className={"login-form"}>
-          <h2>Login</h2>
-          <div>
-            <input type="text" placeholder="Username" required />
-            <FontAwesomeIcon icon={faUser} />
-          </div>
-          <div>
-            <input type="text" placeholder="Username" required />
-            <FontAwesomeIcon icon={faLock} />
-          </div>
-          <div className="forget-password-link">
-            <a href="#">Forget password?</a>
-          </div>
-          <button className="login-btn" type="submit">
+    <div className="login-container">
+      <div className="form-box login">
+        <form>
+          <Typography variant="h4" gutterBottom>
             Login
-          </button>
+          </Typography>
+          <Space direction="vertical" style={{ width: "100%", alignItems: "center" }}>
+            <div className="input-container">
+              <Input placeholder="Username" />
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+            <div className="input-container">
+              <Input.Password placeholder="Password" />
+              <FontAwesomeIcon icon={faLock} />
+            </div>
+            <Typography variant="body2" color="primary">
+              <a href="#">Forgot password?</a>
+            </Typography>
+            <Button variant="contained" color="primary">
+              Login
+            </Button>
+          </Space>
         </form>
       </div>
-    </body>
+    </div>
   )
 }
 
