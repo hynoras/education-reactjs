3f39d4ffa9b8164eb6ee40d9aecf48f070cfed88
Quách Vinh Quang <hynoras1@gmail.com>
2025-02-15 16:04:23 +0700

    continuing styling login form

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,34 +1,40 @@
-import React from "react"
-import { Button, Typography } from "@mui/material"
-import { Input, Space } from "antd"
+import { Typography } from "@mui/material"
+import { Input } from "antd"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons"
 import "./style.scss"
+import LoginButton from "themes/button/LoginButton"
 
 const Login: React.FC = () => {
   return (
     <div className="login-container">
       <div className="form-box login">
         <form>
-          <Typography variant="h4" gutterBottom>
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{
+              margin: "-15px 0 15px"
+            }}
+          >
             Login
           </Typography>
-          <Space direction="vertical" style={{ width: "100%", alignItems: "center" }}>
-            <div className="input-container">
-              <Input placeholder="Username" />
+          <div className={"input-container"}>
+            <Input placeholder="Username" required />
+            <i>
               <FontAwesomeIcon icon={faUser} />
-            </div>
-            <div className="input-container">
-              <Input.Password placeholder="Password" />
+            </i>
+          </div>
+          <div className={"input-container"}>
+            <Input.Password type="password" placeholder="Password" />
+            <i>
               <FontAwesomeIcon icon={faLock} />
-            </div>
-            <Typography variant="body2" color="primary">
-              <a href="#">Forgot password?</a>
-            </Typography>
-            <Button variant="contained" color="primary">
-              Login
-            </Button>
-          </Space>
+            </i>
+          </div>
+          <Typography variant="body2">
+            <a href="#">Forgot password?</a>
+          </Typography>
+          <LoginButton type="submit">Login</LoginButton>
         </form>
       </div>
     </div>
