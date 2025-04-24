80109662bd171831472f4db5bc3db2abf383abb2
unknown <hynoras1@gmail.com>
2025-04-24 11:59:17 +0700

    feat(updateStudentAvatar): add a popover message on success

--- src/shared/components/input/AvatarUpload.tsx
+++ src/shared/components/input/AvatarUpload.tsx
@@ -2,8 +2,11 @@ import { Button, message, Upload, UploadProps } from "antd"
 import { UploadOutlined } from "@ant-design/icons"
 import { useMutation } from "@tanstack/react-query"
 import studentService from "student/services/student/studentService"
+import { DefaultResponse } from "student/models/dtos/defaultResponse"
+import { data } from "react-router"
 
 const AvatarUpload: React.FC<{ studentId: string | undefined }> = ({ studentId }) => {
+  const [messageApi, contextHolder] = message.useMessage()
   const mutation = useMutation({
     mutationFn: (file: File) => {
       const avatar = new FormData()
@@ -12,23 +15,32 @@ const AvatarUpload: React.FC<{ studentId: string | undefined }> = ({ studentId }
     }
   })
 
-  const handleUpload: UploadProps["customRequest"] = ({ file, onSuccess }) => {
+  const successMessage = (data: DefaultResponse | undefined) => {
+    messageApi.open({
+      type: "success",
+      content: data?.message
+    })
+  }
+
+  const handleUpload: UploadProps["customRequest"] = ({ file }) => {
     if (!(file instanceof File)) {
       message.error("Invalid file")
       return
     }
     mutation.mutate(file, {
-      onSuccess: () => {
-        message.success("Image updated successfully!")
-        onSuccess?.({}, new XMLHttpRequest())
+      onSuccess: (data) => {
+        successMessage(data)
       }
     })
   }
 
   return (
-    <Upload customRequest={handleUpload} maxCount={1}>
-      <Button icon={<UploadOutlined />}>Click to Upload</Button>
-    </Upload>
+    <>
+      {contextHolder}
+      <Upload customRequest={handleUpload} maxCount={1} showUploadList={false}>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>
+    </>
   )
 }
 
---
+++ src/shared/components/message/MessageDisplay.tsx
@@ -0,0 +1,16 @@
+import { message } from "antd"
+
+const MessageDisplay: React.FC = () => {
+  const [messageApi, contextHolder] = message.useMessage()
+
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "This is a success message"
+    })
+  }
+
+  return <>{contextHolder}</>
+}
+
+export default MessageDisplay
